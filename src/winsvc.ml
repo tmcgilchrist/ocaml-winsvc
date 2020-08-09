0996faae6f4920b2fa4e8ec66fa221a6938b218b
Romain Beauxis <toots@rastageeks.org>
2020-08-09 01:37:40 -0500

    Switch to dune.

--- src/winsvc.ml
+++ src/winsvc.ml
@@ -1,7 +1,11 @@
+external install : string -> string -> string -> string -> unit
+  = "caml_service_install"
 
-external install : string -> string -> string -> string -> unit = "caml_service_install"
 external remove : string -> unit = "caml_service_remove"
-external run : string -> (unit -> unit) -> (unit -> unit) -> unit = "caml_service_run"
+
+external run : string -> (unit -> unit) -> (unit -> unit) -> unit
+  = "caml_service_run"
+
 external init : unit -> unit = "caml_service_init"
 
 exception Error of string
@@ -10,8 +14,7 @@ let () =
   Callback.register_exception "Service.Error" (Error "register");
   init ()
 
-module type Sig =
-sig
+module type Sig = sig
   val name : string
   val display : string
   val text : string
@@ -19,14 +22,10 @@ sig
   val stop : unit -> unit
 end
 
-module Make(S : Sig) =
-struct
-  let args = 
-    List.map Filename.quote S.arguments
-  let path =
-    String.concat " " (Sys.executable_name :: args)
-  let install () =
-    install S.name S.display S.text path
+module Make (S : Sig) = struct
+  let args = List.map Filename.quote S.arguments
+  let path = String.concat " " (Sys.executable_name :: args)
+  let install () = install S.name S.display S.text path
   let remove () = remove S.name
   let run main = run S.name main S.stop
 end
--- src/winsvc.mli
+++ src/winsvc.mli
@@ -6,26 +6,30 @@
 exception Error of string
 
 (** Signature describing service hosted in this process *)
-module type Sig =
-sig
+module type Sig = sig
   (** Service identifier (unique) *)
   val name : string
+
   (** Service name displayed to user *)
   val display : string
+
   (** Service description *)
   val text : string
+
   (** Service arguments. *)
   val arguments : string list
+
   (** Callback executed when service needs to stop. *)
   val stop : unit -> unit
 end
 
-module Make(S : Sig) :
-sig
+module Make (S : Sig) : sig
   (** Install current executable as Windows service, started with the given arguments *)
   val install : unit -> unit
+
   (** Remove service *)
   val remove : unit -> unit
+
   (** [run main]
     @param main function to run, stdin/stdout not available (will raise exception if used),
                 when [S.stop] is called this function should return as soon as possible *)
